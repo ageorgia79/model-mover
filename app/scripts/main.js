d2c9df0b3fbb5fa5f4812e901c93650c1f678d4e
Andy Georgia <andygeorgia@hotmail.com>
2014-06-10 20:54:24 -0400

    building second view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,6 +19,15 @@ var PhotoCollectionOne = Backbone.Collection.extend({
 
 //COLLECTION ONE ABOVE THIS LINE/////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var PhotoCollectionTwo = Backbone.Collection.extend({
+
+  model: Photo,
+
+  url: 'http://tiny-pizza-server.herokuapp.com/collections/georgiaphotos'
+});
+
+//COLLECTION TWO ABOVE THIS LINE/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var ThumbnailViewOne = Backbone.View.extend({
 
@@ -51,6 +60,38 @@ var ThumbnailViewOne = Backbone.View.extend({
     //$('.modelcage1').append(detail);
   //},
 });
+
+//VIEW ONE ABOVE THIS LINE///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var ThumbnailViewTwo = Backbone.View.extend({
+
+  classname: 'thumbnail',
+
+  thumbnailTemplate2: _.template($('.thumbnail-template-two').text()),
+
+  events: {
+
+    'click .movebutton3': 'jump3',
+    'click .movebutton4': 'jump4',
+    'click .savebutton1': 'save',
+  },
+
+  initialize: function(){
+    $('.modelcage2').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    if (this.model.attributes.hasOwnProperty('url')) {
+      var renderedTemplate = this.thumbnailTemplate2(this.model.attributes);
+      this.$el.html(renderedTemplate);
+    }
+  },
+});
+
+//VIEW TWO ABOVE THIS LINE///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var AppView = Backbone.View.extend({
 
